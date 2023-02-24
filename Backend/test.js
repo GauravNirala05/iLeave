d707c2c4ca9d88521c7ea6a93bd1fddf20cad463
rachna <rachnadewangan92@gmail.com>
2023-02-24 21:08:02 +0530

    js page

--- Backend/test.js
+++ Backend/test.js
@@ -1,6 +1,7 @@
 const date=new Date('2023-03-30')
 const date2= new Date('2023-01-30')
-console.log(date.getMonth());
+// console.log(date.getMonth());
+
 console.log(date2);
 console.log(date-date2);
 console.log(new Date(date-date2));
---
+++ Frontend/assets/js/script.js
@@ -0,0 +1,20 @@
+const fun = () => {
+    try {
+        const div = document.createElement('div')
+        fetch(`http://localhost:4000/faculty/sonu/leaveHistory`, {
+
+        }).then((v) => {
+            console.log(v.status)
+            return v.json()
+        }).then((result) => {
+            console.log(result);
+            console.log(result.hits);
+            div.innerHTML = `<h1>${result.data[0].employee_name}</h1>`
+            document.body.appendChild(div)
+        })
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+fun()
