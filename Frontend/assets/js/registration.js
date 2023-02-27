49cabb9b63809d05a25fbc2e822a35be5a2d1e32
rachna <rachnadewangan92@gmail.com>
2023-02-27 19:23:30 +0530

    Login some changes

--- Frontend/assets/js/registration.js
+++ Frontend/assets/js/registration.js
@@ -1,20 +1,20 @@
 
-var signupform=document.getElementById('signupform')
-signupform.addEventListener('submit',function(event){
-    event.preventDefault()
-    var name=document.getElementById('name').value
-    console.log(name)
-    var username=document.getElementById('username').value
-    console.log(username)
-    var email=document.getElementById('email').value
-    console.log(email)
-    var password=document.getElementById('password').value
-    console.log(password)
-    var confirmpassowrd=document.getElementById('confirmpassword').value
-    console.log(confirmpassowrd)
-    var designation=document.getElementById('designation').value
-    console.log(designation)
-    var department=document.getElementById('department').value
-    console.log(department)
+// var signupform=document.getElementById('signupform')
+// signupform.addEventListener('submit',function(event){
+//     event.preventDefault()
+//     var name=document.getElementById('name').value
+//     console.log(name)
+//     var username=document.getElementById('username').value
+//     console.log(username)
+//     var email=document.getElementById('email').value
+//     console.log(email)
+//     var password=document.getElementById('password').value
+//     console.log(password)
+//     var confirmpassowrd=document.getElementById('confirmpassword').value
+//     console.log(confirmpassowrd)
+//     var designation=document.getElementById('designation').value
+//     console.log(designation)
+//     var department=document.getElementById('department').value
+//     console.log(department)
    
-})
+// })
