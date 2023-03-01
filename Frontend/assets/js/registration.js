c6dc8615ffdafada84a3dab3aa198337fd4f6810
Jitesh <iamjitesh21@gmail.com>
2023-03-01 18:36:17 +0530

    Success window popup

--- Frontend/assets/js/registration.js
+++ Frontend/assets/js/registration.js
@@ -1,40 +1,120 @@
 
-<<<<<<< HEAD
-var signupform=document.getElementById('signupform')
-signupform.addEventListener('submit',function(event){
-    event.preventDefault()
-    var name=document.getElementById('name').values
-    console.log(name)
-    var mob_no=document.getElementById('mob_no').values
-    console.log(mob_no)
-    var email=document.getElementById('email').values
-    console.log(email)
-    var password=document.getElementById('password').values
-    console.log(password)
-    var confirmpassowrd=document.getElementById('confirmpassword').values
-    console.log(confirmpassowrd)
-    var designation=document.getElementById('designation').values
-    console.log(designation)
-    var department=document.getElementById('department').values
-    console.log(department)
+const result = document.querySelector('.result')
+const msg2 = document.querySelector('.msg')
+const login_btn = document.querySelector('.login-btn')
 
-// var signupform=document.getElementById('signupform')
-// signupform.addEventListener('submit',function(event){
-//     event.preventDefault()
-//     var name=document.getElementById('name').value
-//     console.log(name)
-//     var username=document.getElementById('username').value
-//     console.log(username)
-//     var email=document.getElementById('email').value
-//     console.log(email)
-//     var password=document.getElementById('password').value
-//     console.log(password)
-//     var confirmpassowrd=document.getElementById('confirmpassword').value
-//     console.log(confirmpassowrd)
-//     var designation=document.getElementById('designation').value
-//     console.log(designation)
-//     var department=document.getElementById('department').value
-//     console.log(department)
->>>>>>> 49cabb9b63809d05a25fbc2e822a35be5a2d1e32
-   
-// })
+const SamePass = document.querySelector('.SamePass')
+const btn = document.querySelector('.submit-btn')
+const input = document.querySelector('#name')
+const idesignation = document.querySelector('#designation')
+const imob_no = document.querySelector('#mob_no')
+const idepartment = document.querySelector('#department')
+const iemail = document.querySelector('#email')
+const ipassword = document.querySelector('#password')
+const iconpassword = document.querySelector('#Conpassword')
+
+    // Get the modal
+var modal = document.getElementById("myModal");    
+
+    // Get the <span> element that closes the modal
+const span = document.querySelector('.close');
+
+// When the user clicks on the button, open the modal
+span.onclick = function() {
+modal.style.display = "";
+}
+
+
+login_btn.onclick = function() {
+    location.replace("login.html")
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+    }
+
+console.log(ipassword,iconpassword);
+btn.addEventListener('click', async (e) => {
+    e.preventDefault()
+    if (ipassword.value == iconpassword.value) {
+        SamePass.innerHTML = ``
+        // const formAlert = document.querySelector('.form-alert')
+
+        const nameValue = input.value
+        const email = iemail.value
+        const mob_no = imob_no.value
+        const department = idepartment.value
+        const designation = idesignation.value
+        const password = ipassword.value
+        try {
+            const fetcher = await fetch('http://localhost:4000/registration', {
+                method: 'POST',
+                body: JSON.stringify({
+                    name: nameValue,
+                    email: email,
+                    mob_no: mob_no,
+                    department: department,
+                    designation: designation,
+                    password: password
+                }),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+
+            })
+            const { status, data, msg } = await fetcher.json()
+            console.log(data, status, msg)
+            result.innerHTML = `${status}, ${data._id}, ${msg} `
+            if(status=="SUCCESS"){
+                result.innerHTML = `Your Account have been Created`
+                msg2.innerHTML = `${status}`
+                modal.style.display = "block";
+                
+            }
+        } catch (error) {
+            // console.log(error.response)
+            console.log(error.response)
+        }
+
+    }
+    else {
+        SamePass.innerHTML = `Password is not Matching`
+    }
+    
+})
+
+
+btn_log.addEventListener('click', async (e) => {
+    e.preventDefault()
+    console.log('hits');
+    const emaillog = email_log.value
+    const passwordlog = password_log.value
+    console.log(emaillog, passwordlog);
+
+    const loger = await fetch('http://localhost:4000/login', {
+        method: 'POST',
+        body: JSON.stringify({
+            email: emaillog,
+            password: passwordlog
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+
+    })
+    const DATA1 = await loger.json()
+    console.log(DATA1)
+    ihtml = `${DATA1.status}`
+    if (DATA1.status === 'SUCCESS') {
+        ihtml += `${DATA1.data.name}`
+    }
+    if (DATA1.status === 'FAILED') {
+        ihtml += `${DATA1.msg}`
+    }
+    result.innerHTML = ihtml
+
+
+
+})
