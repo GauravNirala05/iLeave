f33f9bacd23dfa1d979a17147dfa5705ee033640
Jitesh <iamjitesh21@gmail.com>
2023-03-01 20:58:29 +0530

    Login window popup

--- Frontend/assets/js/registration.js
+++ Frontend/assets/js/registration.js
@@ -1,7 +1,9 @@
 
 const result = document.querySelector('.result')
 const msg2 = document.querySelector('.msg')
+const war_msg = document.querySelector('.war-msg')
 const login_btn = document.querySelector('.login-btn')
+const ok_btn = document.querySelector('.ok-btn')
 
 const SamePass = document.querySelector('.SamePass')
 const btn = document.querySelector('.submit-btn')
@@ -15,6 +17,7 @@ const iconpassword = document.querySelector('#Conpassword')
 
     // Get the modal
 var modal = document.getElementById("myModal");    
+var modal_err = document.getElementById("myModal-error");    
 
     // Get the <span> element that closes the modal
 const span = document.querySelector('.close');
@@ -22,18 +25,24 @@ const span = document.querySelector('.close');
 // When the user clicks on the button, open the modal
 span.onclick = function() {
 modal.style.display = "";
+modal_err.style.display = "";
 }
 
 
 login_btn.onclick = function() {
     location.replace("login.html")
 }
+
+ok_btn.onclick = function() {
+    modal.style.display = "none";
+}
+
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-    }
+// window.onclick = function(event) {
+//     if (event.target == modal) {
+//         modal.style.display = "none";
+//     }
+//     }
 
 console.log(ipassword,iconpassword);
 btn.addEventListener('click', async (e) => {
@@ -67,11 +76,16 @@ btn.addEventListener('click', async (e) => {
             const { status, data, msg } = await fetcher.json()
             console.log(data, status, msg)
             result.innerHTML = `${status}, ${data._id}, ${msg} `
-            if(status=="SUCCESS"){
+            if(status==="SUCCESS"){
                 result.innerHTML = `Your Account have been Created`
                 msg2.innerHTML = `${status}`
                 modal.style.display = "block";
                 
+            }
+            if(status==="FAILED"){
+                result.innerHTML = `${msg}`
+                war_msg.innerHTML = `${status}`
+                modal.style.display = "block";
             }
         } catch (error) {
             // console.log(error.response)
@@ -83,38 +97,4 @@ btn.addEventListener('click', async (e) => {
         SamePass.innerHTML = `Password is not Matching`
     }
     
-})
-
-
-btn_log.addEventListener('click', async (e) => {
-    e.preventDefault()
-    console.log('hits');
-    const emaillog = email_log.value
-    const passwordlog = password_log.value
-    console.log(emaillog, passwordlog);
-
-    const loger = await fetch('http://localhost:4000/login', {
-        method: 'POST',
-        body: JSON.stringify({
-            email: emaillog,
-            password: passwordlog
-        }),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-
-    })
-    const DATA1 = await loger.json()
-    console.log(DATA1)
-    ihtml = `${DATA1.status}`
-    if (DATA1.status === 'SUCCESS') {
-        ihtml += `${DATA1.data.name}`
-    }
-    if (DATA1.status === 'FAILED') {
-        ihtml += `${DATA1.msg}`
-    }
-    result.innerHTML = ihtml
-
-
-
 })
