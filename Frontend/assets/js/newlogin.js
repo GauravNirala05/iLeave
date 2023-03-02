68cfbcaa8e01bfd4a0a87a2bfb49bca0d638f2ce
Jitesh <iamjitesh21@gmail.com>
2023-03-02 20:15:59 +0530

    changes

--- Frontend/assets/js/newlogin.js
+++ Frontend/assets/js/newlogin.js
@@ -9,3 +9,13 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
+
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
