fea1eca172e731a03f34f8d702dfe9291d125d9e
rachna <rachnadewangan92@gmail.com>
2023-03-04 09:15:51 +0530

    js

--- Frontend/assets/js/applyleave.js
+++ Frontend/assets/js/applyleave.js
@@ -1,77 +1,97 @@
-const result = document.querySelector('.result')
-const apply_btn = document.querySelector('.apply_submit')
+const btn_apply = document.querySelector('.btn_apply')
 const apply_name = document.querySelector('#name')
-const apply_startdate = document.querySelector('#startdate')
+const apply_contactno = document.querySelector('#mob_no')
+const apply_fromdate = document.querySelector('#fromdate')
+const apply_todate = document.querySelector('#todate')
 const apply_totaldays = document.querySelector('#totaldays')
-const apply_contactno = document.querySelector('#contactno')
-const apply_enddate = document.querySelector('#enddate')
-const apply_type = document.querySelector('#leavetype')
 const reference1 = document.querySelector('#reference1')
 const reference2 = document.querySelector('#reference2')
-const reference3 = document.querySelector('#reference2')
+const reference3 = document.querySelector('#reference3')
 const reference4 = document.querySelector('#reference4')
+const apply_type = document.querySelector('#leavetype')
 const apply_reason = document.querySelector('#reason')
-// const apply_checkbox = document.querySelector('#checkbox')
-//console.log(ipassword,iconpassword);
-apply_btn.addEventListener('click', async (e) => {
+// const token =localStorage.getItem('token')
+btn_apply.addEventListener('click', async (e) => {
     // ihtml=``
-    
     e.preventDefault()
-    
         // const formAlert = document.querySelector('.form-alert')
         const name = apply_name.value
-        const startdate = apply_startdate.value
-        
-        const totaldays = apply_totaldays.value
-        const enddate = apply_enddate.value
         const contactno = apply_contactno.value
-        const leavetype = apply_type.value
+        const fromdate = apply_fromdate.value
+        const todate = apply_todate.value
+        const total_days = apply_totaldays.value
         const ref1 = reference1.value
         const ref2 = reference2.value
         const ref3 = reference3.value
         const ref4 = reference4.value
+        const leave_type = apply_type.value
         const reason = apply_reason.value
         // totaldays.innerHTML=`${apply_enddate.value-apply_startdate.value}`
-        console.log(name, startdate,enddate,totaldays,contactno
-            ,leavetype,ref1,ref2,ref3,ref4,reason);
-            
+        console.log(name, contactno,fromdate,todate,total_days,
+            ref1,ref2,ref3,ref4,leave_type,reason);
+        
+        let token
+        localStorage.getItem('token')
+        console.log(localStorage)
         // const checkbox = apply_checkbox.value
-        try {
-            let id ;
-            id=localStorage.getItem("id",id)
-            const fetcher = await fetch(`http://localhost:4000/applyLeave/${id}`, {
+      if(token){ 
+         try {
+            // let id ;
+            // id=localStorage.getItem("id",id)
+            const fetcher = await fetch(`http://localhost:4000/applyLeave`, {
                 method: 'POST',
                 body: JSON.stringify({
                     employee_name: name,
-                    to_date: startdate,
-                    total_days: totaldays,
-                    from_date: enddate,
                     contect_no: contactno,
-                    leave_type: leavetype,
+                    from_date: fromdate,
+                    to_date:todate,
+                    total_days: total_days,
                     reference1: ref1,
                     reference2: ref2,
                     reference3: ref3,
                     reference4: ref4,
+                    leave_type: leave_type,
                     discription: reason,
-                    // password: checkbox
 
                 }),
                 headers: {
-                    'Content-Type': 'application/json'
+                    'Content-Type': 'application/json',
+                    'Authorization':`Bearer ${token}`
                 }
                
-                
+   
             })
-           
-            const { status, data, msg } = await fetcher.json()
-            console.log(data, status, msg)
-            const h5 = document.createElement('span')
-            h5.innerHTML = `${status}, ${data._id}, ${msg} `
-            result.appendChild(h5)
+            if (!fetcher.ok) {
+                const { msg, error } = await fetcher.json()
+                throw Error(fetcher.status + " " + error)
+            }
+            console.log(localStorage)
+            const { token, msg } = await fetcher.json()
+            alert(`${msg}`)
+            setTimeout(()=>{
+                location.replace("Dasboard.html")
+            },3000)
+            apply_name.value=``
+            apply_contactno.value=``
+            apply_fromdate.value=``
+            apply_todate.value=``
+            apply_totaldays.value=``
+            reference1.value=``
+            reference2.value=``
+            reference3.value=``
+            reference4.value=``
+            apply_type.value=``
+            apply_reason.value=``
+
+            // const { status, data, msg } = await fetcher.json()
+            // console.log(data, status, msg)
+            // const h5 = document.createElement('span')
+            // h5.innerHTML = `${status}, ${data._id}, ${msg} `
+            // result.appendChild(h5)
         } catch (error) {
-            // console.log(error.response)
-            console.log(error.response)
+            console.log('Error===>', error)
         }
+    }
 })
 
 
