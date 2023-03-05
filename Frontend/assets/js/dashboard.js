6ed9cd6ea1b1bed034c8d06a4d2f518933d47769
Jitesh <iamjitesh21@gmail.com>
2023-03-05 14:14:01 +0530

    HOD update

--- Frontend/assets/js/dashboard.js
+++ Frontend/assets/js/dashboard.js
@@ -1,5 +1,6 @@
 const getuser=async ()=>{
   const token =localStorage.getItem('token')
+
   if(token){
     try {
       
@@ -48,6 +49,74 @@ const getuser=async ()=>{
         
       }
       document.getElementById("profile").innerHTML=ihtml
+
+      let hodhtml=``
+
+      // once login is implemeted it will be == insted of !=
+      if (userData.data.designation !="HOD"){
+        console.log(userData.data.designation)
+  
+        hodhtml=`<div class="projects">
+        <div class="card">
+          <div class="card-header">
+            <h2>On Leave</h2>
+            <button>See all <span class="fa fa-arrow-down"></span> </button>
+            <!--<button>See all <span class="fa fa-arrow-right"></span> </button>-->
+          </div>
+          <div class="card-body">
+            <div class="table-responsive">
+              <table width="100%">
+                <thead>
+                  <tr>
+                    <td></td>
+                    <td>Name</td>
+                    <td>To-From</td>
+                    <td>Department</td>
+                    <td>Contact</td>
+
+
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>
+                      <i class="fa fa-check-circle" style="font-size:24px; margin-right:15px"></i>
+
+                    </td>
+                    <td style="text-transform:capitalize">Prof. Kunal Sharma</td>
+                    <td>
+                      <h6 class="text-dark text-left">25 Jul- 27 Jul</h6>
+                    </td>
+                    <td>Mining  </td>
+                    <td>99999999  </td>
+                    </tr>
+                  <tr>
+                    <td>
+                      <i class="fa fa-check-circle" style="font-size:24px; margin-right:15px"></i>
+                    </td>
+                    <td>Prof. Surbhi Verma</td>
+                    <td>
+                      <h6 class="text-dark text-left">25 Jul- 27 Jul</h6>
+                    </td>
+                    <td>Civil </td>
+                    <td>88888888 </td>
+
+                  </tr>
+
+
+                </tbody>
+
+              </table>
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>`
+      }
+      document.getElementById("only_auth").innerHTML=hodhtml
+
 
     } catch (error) {
       console.log(error);
