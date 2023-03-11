bba459bf4e599834678287f37efc8a73196ce72e
Jitesh <iamjitesh21@gmail.com>
2023-03-11 13:51:36 +0530

    complete profile update

--- Frontend/assets/js/completeProfile.js
+++ Frontend/assets/js/completeProfile.js
@@ -13,3 +13,49 @@ imageUpload.addEventListener("change", function() {
     reader.readAsDataURL(file);
   }
 });
+
+
+const save = document.querySelector('.save')
+const newname = document.querySelector('.newname')
+const mob_no = document.querySelector('.number')
+const department = document.querySelector('.department')
+const designation = document.querySelector('.designation')
+
+save.addEventListener('click', async (e) => {
+    const token =localStorage.getItem('token')
+    e.preventDefault()
+        const name = newname.value
+        const mob = mob_no.value
+        const depart = department.value
+        const desig = designation.value
+        try {
+            const fetcher = await fetch('http://localhost:4000/Completeprofile', {
+                method: 'PATCH',
+                body: JSON.stringify({
+                    name: name,
+                    mob_no: mob_no,
+                    department: department,
+                    designation: designation
+                }),
+                headers: {
+                    'Content-Type': 'application/json',
+                    "Authorization": "Bearer " + token
+                }
+
+            })
+            if (!fetcher.ok) {
+                const status = fetcher.status
+                console.log(status)
+                const { msg, error } = await fetcher.json()
+                console.log(msg);
+                throw Error(`${status}`)
+            }
+            const { msg } = await fetcher.json()
+            alert(`${msg}`)
+        } catch (error) {
+            console.log(error)
+        }
+
+    }
+
+)
