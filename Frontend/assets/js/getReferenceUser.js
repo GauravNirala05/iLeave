714c3ae003fc7502687ce3d11b9a2daaaa5fb32f
rachna <rachnadewangan92@gmail.com>
2023-03-14 21:23:06 +0530

    getReferenceUser.js

---
+++ Frontend/assets/js/getReferenceUser.js
@@ -0,0 +1,32 @@
+const getReferenceUser=async()=>{
+    const token =localStorage.getItem('token')
+    try{
+        const getRefUser=await fetch('/getReferenceUser',{
+            headers:{
+                'Authorization':`Bearer ${token}`
+            }
+            
+        })
+        console.log("Running")
+        if(!getRefUser.ok){
+            const users=await getRefUser.json()
+            throw Error(userData.msg)
+        }
+        const users=await getRefUser.json()
+        console.log(users)
+        let ihtml=``
+        for (item in users){
+            ihtml=` <option>${(users.hits)}</option>
+            <option>Person 2</option>
+            <option>Person 3</option>
+            <option>Person 4</option>
+            `
+        }
+        document.getElementById("reference1").innerHTML=ihtml
+
+    }
+    catch(error){
+        console.log(error)
+    }
+}
+getReferenceUser()
