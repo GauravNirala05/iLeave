4e245c04b23f6b80ed9e9386606e036b57f481a6
Gaurav <niralagaurav14@gmail.com>
2023-03-15 17:29:40 +0530

    ok

--- Frontend/assets/js/applyleave.js
+++ Frontend/assets/js/applyleave.js
@@ -1,20 +1,9 @@
 
 const button_apply = document.querySelector('.btn_apply')
-const total_days = document.querySelector('.totaldays')
-const fromdate = document.querySelector('.fromdate').value
-const todate = document.querySelector('.todate').value
-
-if (fromdate != undefined && todate != undefined) {
-    const date2 = new Date(fromdate)
-    const date1 = new Date(todate)
-    console.log(date1 - date2);
-    const totaldays = (((date1 - date2) / (1000 * 60 * 60 * 24)) + 1)
-    console.log(totaldays);
-    total_days.ariaPlaceholder = totaldays
-}
 
 button_apply.addEventListener('click', async (e) => {
     e.preventDefault()
+
     const contactno = document.querySelector('.mob_no').value
     const fromdate = document.querySelector('.fromdate').value
     const todate = document.querySelector('.todate').value
@@ -24,7 +13,7 @@ button_apply.addEventListener('click', async (e) => {
     const reference4 = document.querySelector('.reference4').value
     const leave_type = document.querySelector('.leavetype').value
     const reason = document.querySelector('.reason').value
-    console.log(leave_type,reason,contactno,fromdate,todate);
+
     if (!fromdate || !todate) {
         throw Error(`please provide dates first`)
     }
