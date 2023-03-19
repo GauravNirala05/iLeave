6de2457aea2f422faf2081ea7dab4be5b8fc06fc
Jitesh <iamjitesh21@gmail.com>
2023-03-19 15:32:39 +0530

    status nav

--- Frontend/assets/js/status.js
+++ Frontend/assets/js/status.js
@@ -1,3 +1,26 @@
+var content1 = document.getElementById("content-1");
+var content2 = document.getElementById("content-2");
+var button1 = document.getElementById("toggle-button-1");
+var button2 = document.getElementById("toggle-button-2");
+
+content1.style.display = "block";
+content2.style.display = "none";
+button1.classList.add("active");
+
+
+function toggleContentstatus() {
+  content1.style.display = "block";
+  content2.style.display = "none";
+  button1.classList.add("active");
+  button2.classList.remove("active");
+}
+
+function toggleContenthistory() {
+  content1.style.display = "none";
+  content2.style.display = "block";
+  button1.classList.remove("active");
+  button2.classList.add("active");
+}
 
 const getleavestatus = async () => {
   const stat = []
@@ -339,54 +362,6 @@ function toggleDropdown() {
   }
 }
 
-function openPopup() {
-  document.getElementById("popup").style.display = "block";
-}
-
-function openPopup2() {
-  document.getElementById("popup2").style.display = "block";
-}
-
-function closePopup() {
-  document.getElementById("popup").style.display = "none";
-}
-
-function confirm_logout() {
-  localStorage.removeItem('token');
-  localStorage.removeItem('designation');
-
-  location.replace("index.html")
-}
-function complete_profile() {
-  location.replace("complete_profile.html")
-}
-function login() {
-  location.replace("login.html")
-}
-
-var content1 = document.getElementById("content-1");
-var content2 = document.getElementById("content-2");
-var button1 = document.getElementById("toggle-button-1");
-var button2 = document.getElementById("toggle-button-2");
-
-content1.style.display = "block";
-content2.style.display = "none";
-button1.classList.add("active");
-
-
-function toggleContentstatus() {
-  content1.style.display = "block";
-  content2.style.display = "none";
-  button1.classList.add("active");
-  button2.classList.remove("active");
-}
-
-function toggleContenthistory() {
-  content1.style.display = "none";
-  content2.style.display = "block";
-  button1.classList.remove("active");
-  button2.classList.add("active");
-}
 
 
 window.onload = function () {
