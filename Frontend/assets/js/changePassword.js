fcb1db52bb0332056772b7b451bf280225b49edf
Gaurav <niralagaurav14@gmail.com>
2023-03-26 16:26:13 +0530

    ok

--- Frontend/assets/js/changePassword.js
+++ Frontend/assets/js/changePassword.js
@@ -11,101 +11,61 @@ function showPopup(msg) {
 function hidePopup() {
     myPopup.style.display = "none";
 }
-async function resetPassword() {
-    try {
-        const resetPassword = document.querySelector('#resetPassword').value
-        const resetPasswordConfirm = document.querySelector('#resetPasswordConfirm').value
-        if (resetPassword === resetPasswordConfirm) {
-            const resetPasswordData = await fetch(`/forgotPassword/resetPass`, {
-                method: 'PATCH',
-                body: JSON.stringify({
-                    password: resetPasswordConfirm
-                }),
-                headers: {
-                    'Content-type': 'application/json',
-                    'Authorization': `Bearer ${token}`
-                }
-            })
-            if (!resetPasswordData.ok) {
-                const status = resetPasswordData.status
-                const { msg } = await resetPasswordData.json()
-                throw Error(`${status} ${msg}`)
-            }
-            else {
-                const { msg } = await resetPasswordData.json()
-                showPopup(msg)
-                localStorage.removeItem('token')
-                location.replace("login.html")
-            }
-        } else {
-            throw Error(`Passwords are not matching...`)
-        }
 
-    } catch (error) {
-        showPopup(error)
-    }
-}
 const resetPasswordBtn = document.querySelector('#resetPasswordBtn')
 
-resetPasswordBtn.addEventListener('click', async (e) => {
-    e.preventDefault()
-    const forgotPassEmail = document.querySelector('#resetPasswordEmail').value
-    console.log(forgotPassEmail);
-    try {
-        const forgotPassData = await fetch(`/forgotPassword`, {
-            method: 'POST',
-            body: JSON.stringify({
-                email: forgotPassEmail
-            }),
-            headers: {
-                'Content-type': 'application/json'
-            }
-        })
-        if (!forgotPassData.ok) {
-            const status = forgotPassData.status
-            const { msg } = await forgotPassData.json()
-            throw Error(`${status} ${msg}`)
-        }
-        else {
-            const { userid, msg } = await forgotPassData.json()
-            showPopup(msg)
-            document.querySelector('#form').innerHTML = `
-                <img class="logo" src="images/name.svg">
-                <span class="row mb-5">
-                <h2>Reset Password</h2>
-                </span>
-
-                <input type="number" id="resetPasswordOTP" class="email" placeholder="OTP" />
-
-                <button id="OtpVerification" class="mt-4 mb-5">Submit</button>
-                <button class="mt-4 mb-5" id="sendOTPAgain">Re-Send OTP</button>
-                <a target="_blank" href="login.html">Sign In and Register</a>
-            `
-        }
-
-    } catch (error) {
-        showPopup(error)
-    }
-})
-const sendOTPAgain=document.querySelector('#sendOTPAgain')
-if(sendOTPAgain){
-    sendOTPAgain.addEventListener('click',async (e)=>{
+if (resetPasswordBtn) {
+    resetPasswordBtn.addEventListener('click', async (e) => {
         e.preventDefault()
-        
+        if (document.querySelector('#resetPasswordEmail')) {
+            const forgotPasswordEmail = document.querySelector('#resetPasswordEmail').value
+            localStorage.setItem('forgetEmail', forgotPasswordEmail)
+        }
+        const forgotPassEmail = localStorage.getItem('forgetEmail')
+        if (forgotPassEmail) {
+            try {
+                const forgotPassData = await fetch(`/forgotPassword`, {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        email: forgotPassEmail
+                    }),
+                    headers: {
+                        'Content-type': 'application/json'
+                    }
+                })
+                if (!forgotPassData.ok) {
+                    const status = forgotPassData.status
+                    const { msg } = await forgotPassData.json()
+                    throw Error(`${status} ${msg}`)
+                }
+                else {
+                    const { userid, msg } = await forgotPassData.json()
+                    localStorage.setItem('userID', userid)
+                    showPopup(msg)
+                    setTimeout(() => {
+                        location.replace("verifyOTP.html")
+                    }, 2000);
+                }
+
+            } catch (error) {
+                showPopup(error)
+            }
+        }
     })
 }
+
 const OtpVerification = document.querySelector('#OtpVerification')
 if (OtpVerification) {
     OtpVerification.addEventListener('click', async (e) => {
         e.preventDefault()
         try {
-            console.log((id));
+            const userID = localStorage.getItem('userID')
             const resetPasswordOTP = document.querySelector('#resetPasswordOTP').value
             console.log(resetPasswordOTP);
             const verifyOTPData = await fetch(`/forgotPassword/verifyOTP`, {
                 method: 'POST',
                 body: JSON.stringify({
-                    id: id,
+                    id: userID,
                     OTP: resetPasswordOTP
                 }),
                 headers: {
@@ -115,27 +75,78 @@ if (OtpVerification) {
             if (!verifyOTPData.ok) {
                 const status = verifyOTPData.status
                 const { msg } = await verifyOTPData.json()
+                localStorage.removeItem('userID')
                 throw Error(`${status} ${msg}`)
+
             }
             else {
+                localStorage.removeItem('userID')
                 const { token, msg } = await verifyOTPData.json()
                 showPopup(msg)
                 localStorage.setItem('token', token)
-                document.querySelector('#form').innerHTML = `
-                <img class="logo" src="images/name.svg">
-                <span class="row mb-5">
-                <h2>Reset Password</h2>
-                </span>
-    
-                <input required type="String" id="resetPassword" class="email" placeholder="New Password" />
-                <input required type="String" id="resetPasswordConfirm" class="email" placeholder="Confirm Password" />
-    
-                <button class="mt-4 mb-5" id="resetPassword">Reset Password</button>
-                <a target="_blank" href="login.html">Sign In and Register</a>
-                `
+                setTimeout(() => {
+                    location.replace("changePassword.html")
+                }, 2000);
             }
         } catch (error) {
             showPopup(error)
+            setTimeout(() => {
+                location.replace("sentOTP.html")
+            }, 5000);
+
         }
     })
 }
+
+const changePassword = document.querySelector('#changePassword')
+if (changePassword) {
+    changePassword.addEventListener('click', async (e) => {
+        e.preventDefault()
+        try {
+            const token=localStorage.getItem('token')
+            const resetPassword = document.querySelector('#resetPassword').value
+            const resetPasswordConfirm = document.querySelector('#resetPasswordConfirm').value
+            if (resetPassword == resetPasswordConfirm) {
+                const resetPasswordData = await fetch(`/forgotPassword/resetPass`, {
+                    method: 'PATCH',
+                    body: JSON.stringify({
+                        password: resetPasswordConfirm
+                    }),
+                    headers: {
+                        'Content-type': 'application/json',
+                        'Authorization': `Bearer ${token}`
+                    }
+                })
+                if (!resetPasswordData.ok) {
+                    const status = resetPasswordData.status
+                    const { msg } = await resetPasswordData.json()
+                    localStorage.removeItem('token')
+                    throw Error(`${status} ${msg}`)
+                }
+                else {
+                    localStorage.removeItem('token')
+                    const { msg } = await resetPasswordData.json()
+                    showPopup(msg)
+                    location.replace("login.html")
+                }
+            } else {
+                throw Error(`Passwords are not matching...`)
+            }
+
+        } catch (error) {
+            showPopup(error)
+            setTimeout(() => {
+                location.replace("sentOTP.html")
+            }, 5000);
+        }
+    })
+}
+
+
+window.onload = function () {
+    document.getElementById('loading-screen').style.display = 'block';
+};
+function off() {
+    document.getElementById('loading-screen').hidden = 'true';
+}
+off()
